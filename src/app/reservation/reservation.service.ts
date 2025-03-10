e6d57e19032b54c1c71448f71a6bd28ddaea44a0
Nthabeleng <nthabielle@live.com>
2025-03-10 19:02:42 +0200

    Adding edit functionality (#10)
    * Adding ng-template and local reference
    
    * Adding edit functionality

--- src/app/reservation/reservation.service.ts
+++ src/app/reservation/reservation.service.ts
@@ -25,6 +25,8 @@ export class ReservationService {
    }
 
    addReservation(reservation: Reservation):void {
+     reservation.id = Date.now().toString()
+
      this.reservations.push(reservation)
      localStorage.setItem("reservations", JSON.stringify(this.reservations))
      console.log(this.reservations)
@@ -36,10 +38,10 @@ export class ReservationService {
     localStorage.setItem("reservations", JSON.stringify(this.reservations))
    }
 
-  updateReservation(updatedReservation:Reservation): void {
-    let index = this.reservations.findIndex(res => res.id === updatedReservation.id)
-    this.reservations[index] = updatedReservation
-    localStorage.setItem("reservations", JSON.stringify(this.reservations))
-   }
+  updateReservation(id: string, updatedReservation: Reservation): void {
+    let index = this.reservations.findIndex(res => res.id === id);
+    this.reservations[index] = updatedReservation;
+    localStorage.setItem("reservations", JSON.stringify(this.reservations));
+  }
 
 }
